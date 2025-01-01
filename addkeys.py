399efb1320a7a55e2d757736b7d17a94734ba358
FunnyBiu <549308442@qq.com>
2025-01-01 19:24:32 +0800

    Update and rename dedeem.py to addkeys.py

--- addkeys.py
+++ addkeys.py
@@ -32,7 +32,7 @@ games_line=f.readlines()
 f.close()
 for game_line in games_line:
     found=0
-    if "请求失败" in game_line:
+    if "请求失败" in game_line:  #删除无key行
         continue
     region,url,name,key=game_line.split("\t")
     encodeurl=urllib.parse.quote(url)
@@ -40,7 +40,7 @@ for game_line in games_line:
     cookies=cookies,
     headers=headers,
 )
-    if len(response.json().get("result").get("content"))==1:
+    if len(response.json().get("result").get("content"))==1:  #返回列表只有一个游戏，可能匹配，按最低价上架游戏并人工提示
         print(name,key,response.json().get("result").get("content")[0].get("gameName"),"maybegood")
         addgame(response.json().get("result").get("content")[0].get('id'),key,response.json().get("result").get("content")[0].get('keyPrice'),region)
         continue
@@ -48,8 +48,8 @@ for game_line in games_line:
         if gamelist.get("gameName")== name:
             print(name,key,"good")
             addgame(gamelist.get('id'),key,gamelist.get('keyPrice'),region)
-            found=1  
+            found=1   #从返回列表里面找到匹配游戏则退出循环，按最低价上架游戏
             break
     if found==0:
-        print(region,url,name,key,"bad")
+        print(region,url,name,key,"bad")#没有匹配游戏则提示
 
